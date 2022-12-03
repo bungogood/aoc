1e76adf3ec0449f2a7981739d26f5cedf5c721f7
Bungogood <github@bungogood.com>
2022-12-03 20:08:07 +0000

    1st 2022

---
+++ 2022/day01.hs
@@ -0,0 +1,24 @@
+import System.IO  
+import Control.Monad
+import Data.List.Split
+import Data.List
+
+toNum :: String -> Int
+toNum s = read s :: Int
+
+elfCals :: String -> [[Int]]
+elfCals = map (map toNum . splitOn "\n") . splitOn "\n\n"
+
+bestElf :: [[Int]] -> Int
+bestElf = maximum . map sum
+
+topElves :: Int -> [[Int]] -> Int
+topElves n = sum . take n . reverse . sort . map sum
+
+main = do
+  handler <- openFile "input/day01.txt" ReadMode
+  contents <- hGetContents handler
+  let elfData = elfCals contents
+  print (bestElf elfData)
+  print (topElves 3 elfData)
+  hClose handler
