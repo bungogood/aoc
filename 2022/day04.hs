b6c609f2e5c6a00258a0e1e77ba148767b0fe4cd
Bungogood <github@bungogood.com>
2022-12-04 12:45:33 +0000

    4th 2022

---
+++ 2022/day04.hs
@@ -0,0 +1,34 @@
+import System.IO  
+import Control.Monad
+import Data.List.Split
+import Data.List
+import Data.Char
+
+toNum :: String -> Int
+toNum s = read s :: Int
+
+toList :: (Int, Int) -> [Int]
+toList (x, y) = [x..y]
+
+toTuple :: [a] -> (a, a)
+toTuple [x, y] = (x, y)
+
+toRange :: String -> (Int, Int)
+toRange = toTuple . map toNum . splitOn "-"
+
+readRanges :: String -> [((Int, Int), (Int, Int))]
+readRanges = map (toTuple . map toRange . splitOn ",") . splitOn "\n"
+
+inside :: ((Int, Int), (Int, Int)) -> Bool
+inside ((a, b), (c, d)) = (a <= c && d <= b) || (c <= a && b <= d)
+
+overlap :: ((Int, Int), (Int, Int)) -> Bool
+overlap ((a, b), (c, d)) = (c >= a || d >= a) && (b >= c || b >= d)
+
+main = do
+  handler <- openFile "input/day04.txt" ReadMode
+  contents <- hGetContents handler
+  let ranges = readRanges contents
+  print (length $ filter inside  ranges)
+  print (length $ filter overlap ranges)
+  hClose handler
