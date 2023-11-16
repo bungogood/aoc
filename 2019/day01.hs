4f4f3269ba4e998157594cc25951e9ea71c237a5
Jonathan Good <github@bungogood.com>
2023-11-16 19:25:38 +0000

    2019 1st

---
+++ 2019/day01.hs
@@ -0,0 +1,26 @@
+import System.IO
+import Data.List
+
+-- https://adventofcode.com/2019/day/1
+
+toNum :: String -> Int
+toNum s = read s :: Int
+
+findMasses :: String -> [Int]
+findMasses = map toNum . lines
+
+toFuel :: Int -> Int
+toFuel x = x `div` 3 - 2
+
+calFuel :: Int -> Int
+calFuel x
+  | x <= 0 = 0
+  | otherwise = x + calFuel (toFuel x)
+
+main = do
+  handler <- openFile "input/day01.txt" ReadMode
+  contents <- hGetContents handler
+  let masses = findMasses contents
+  print (sum $ map toFuel masses)
+  print (sum $ map (calFuel . toFuel) masses)
+  hClose handler
