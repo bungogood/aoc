2af37d03165f0da5505533b0ad61fb51a25a3d8c
Jonathan Good <github@bungogood.com>
2023-11-21 15:05:49 +0000

    2017 1st

---
+++ 2017/day01.hs
@@ -0,0 +1,22 @@
+import System.IO
+import Data.List
+import Data.Char (digitToInt)
+
+-- https://adventofcode.com/2017/day/1
+
+toDigits :: String -> [Int]
+toDigits = map digitToInt . head . lines
+
+offset :: Int -> [Int] -> [(Int, Int)]
+offset o xs = zip xs (drop o xs ++ take o xs)
+
+counter :: [(Int, Int)] -> Int
+counter = sum . map fst . filter (uncurry (==))
+
+main = do
+  handler <- openFile "input/day01.txt" ReadMode
+  contents <- hGetContents handler
+  let digits = toDigits contents
+  print (counter $ offset 1 digits)
+  print (counter $ offset (div (length digits) 2) digits)
+  hClose handler
