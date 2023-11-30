88b4cadc6fa75dcc544fd6e0f8accfdc7f5ec81b
Jonathan Good <github@bungogood.com>
2023-11-30 16:58:01 +0000

    2015 1st

---
+++ 2015/day01.hs
@@ -0,0 +1,21 @@
+import System.IO
+
+-- https://adventofcode.com/2015/day/1
+
+walk :: Int -> String -> Int
+walk f ('(':xs) = walk (f + 1) xs
+walk f (')':xs) = walk (f - 1) xs
+walk f _ = f
+
+basement :: Int -> Int -> String -> Int
+basement f p ('(':xs) = basement (f + 1) (p + 1) xs
+basement f p (')':xs) | f == 0 = p + 1
+                      | otherwise = basement (f - 1) (p + 1) xs
+basement f p _ = p + 1
+
+main = do
+  handler <- openFile "input/day01.txt" ReadMode
+  contents <- hGetContents handler
+  print (walk 0 contents)
+  print (basement 0 0 contents)
+  hClose handler
