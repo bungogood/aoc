8235c361f2ec79b95231a3b09db91c42e3d241eb
Jonathan Good <github@bungogood.com>
2023-11-30 17:07:23 +0000

    2015 2nd

---
+++ 2015/day02.hs
@@ -0,0 +1,24 @@
+import System.IO
+import Data.List.Split
+
+-- https://adventofcode.com/2015/day/2
+
+toNum :: String -> Int
+toNum s = read s :: Int
+
+toBoxes :: String -> [(Int, Int, Int)]
+toBoxes = map ((\[l, w, h] -> (toNum l, toNum w, toNum h)) . splitOn "x") . lines
+
+wrapping :: (Int, Int, Int) -> Int
+wrapping (l, w, h) = 2 * l * w + 2 * w * h + 2 * h * l + minimum [l * w, w * h, h * l]
+
+ribbon :: (Int, Int, Int) -> Int
+ribbon (l, w, h) = minimum [2 * l + 2 * w, 2 * w + 2 * h, 2 * h + 2 * l] + l * w * h
+
+main = do
+  handler <- openFile "input/day02.txt" ReadMode
+  contents <- hGetContents handler
+  let boxes = toBoxes contents
+  print (sum . map wrapping $ boxes)
+  print (sum . map ribbon $ boxes)
+  hClose handler
