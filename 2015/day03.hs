8465b8a3791fd372d11beaabc21eae0191e59e58
Jonathan Good <github@bungogood.com>
2023-12-02 12:06:18 +0000

    Switching to qualified imports

--- 2015/day03.hs
+++ 2015/day03.hs
@@ -1,6 +1,7 @@
 import System.IO
 import Data.List.Split
-import Data.Set (Set, member, insert, size, singleton, union)
+import Data.Set (Set)
+import qualified Data.Set as Set
 
 -- https://adventofcode.com/2015/day/3
 
@@ -11,10 +12,10 @@ step (x, y) '^' = (x, y + 1)
 step (x, y) 'v' = (x, y - 1)
 
 walk :: (Int, Int) -> String -> Set (Int, Int)
-walk p = walkRec p (singleton p)
+walk p = walkRec p (Set.singleton p)
 
 walkRec :: (Int, Int) -> Set (Int, Int) -> String -> Set (Int, Int)
-walkRec p v (d:ds) = walkRec (step p d) (insert (step p d) v) ds
+walkRec p v (d:ds) = walkRec (step p d) (Set.insert (step p d) v) ds
 walkRec p v [] = v
 
 splitAlt :: String -> (String, String)
@@ -27,6 +28,6 @@ main = do
   handler <- openFile "input/day03.txt" ReadMode
   contents <- hGetContents handler
   let (santa, robo) = splitAlt contents
-  print (size $ walk (0, 0) contents)
-  print (size $ walk (0, 0) santa `union` walk (0, 0) robo)
+  print (Set.size $ walk (0, 0) contents)
+  print (Set.size $ walk (0, 0) santa `Set.union` walk (0, 0) robo)
   hClose handler
--- 2018/day01.hs
+++ 2018/day01.hs
@@ -1,5 +1,6 @@
 import System.IO
-import Data.Set (Set, insert, member, empty)
+import Data.Set (Set)
+import qualified Data.Set as Set
 
 -- https://adventofcode.com/2018/day/1
 
@@ -12,8 +13,8 @@ toChange ('-':xs) = -toNum xs
 
 repeated :: Int -> Set Int -> [Int] -> [Int] -> Int
 repeated value seen [] ls = repeated value seen ls ls
-repeated value seen (c:cs) ls | member value seen = value
-                              | otherwise = repeated (value + c) (insert value seen) cs ls
+repeated value seen (c:cs) ls | Set.member value seen = value
+                              | otherwise = repeated (value + c) (Set.insert value seen) cs ls
 
 main :: IO ()
 main = do
@@ -21,5 +22,5 @@ main = do
   contents <- hGetContents handler
   let changes = map toChange $ lines contents
   print (sum changes)
-  print (repeated 0 empty [] changes)
+  print (repeated 0 Set.empty [] changes)
   hClose handler
