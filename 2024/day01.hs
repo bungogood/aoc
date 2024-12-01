9b41af1dc51e58773acdeab3ced4290129d53014
Jonathan Good <github@bungogood.com>
2024-12-01 12:05:38 +0000

    2024 1st tidy

--- 2024/day01.hs
+++ 2024/day01.hs
@@ -1,34 +1,32 @@
 import Data.List (sort)
 import Data.Map qualified as Map
 import System.Environment (getArgs)
-import System.IO (readFile)
+import System.IO (hPutStrLn, readFile, stderr)
 
 -- https://adventofcode.com/2024/day/1
 
-toNum :: String -> Int
-toNum s = read s
-
 parse :: String -> [(Int, Int)]
-parse = map (parse' . words) . lines
-  where
-    parse' [a, b] = (toNum a, toNum b)
+parse = map ((\[a, b] -> (read a, read b)) . words) . lines
 
-reform :: [(Int, Int)] -> ([Int], [Int])
-reform = foldr (\(a, b) (accA, accB) -> (a : accA, b : accB)) ([], [])
-
-sortedDiffs :: ([Int], [Int]) -> [Int]
-sortedDiffs (a, b) = map abs $ zipWith (-) (sort a) (sort b)
+sumSortedDiffs :: [Int] -> [Int] -> Int
+sumSortedDiffs a b = sum $ zipWith (\a b -> abs (a - b)) (sort a) (sort b)
 
 countOccurances :: [Int] -> Map.Map Int Int
-countOccurances = Map.fromListWithKey (\_ a b -> a + b) . map (\x -> (x, 1))
+countOccurances = Map.fromListWith (+) . map (,1)
 
-similarity :: Map.Map Int Int -> [Int] -> [Int]
-similarity lookup = map (\x -> x * Map.findWithDefault 0 x lookup)
+similarityScore :: [Int] -> [Int] -> Int
+similarityScore xs = sum . map (\y -> y * lookup y)
+  where
+    occurrences = countOccurances xs
+    lookup y = Map.findWithDefault 0 y occurrences
 
 main :: IO ()
 main = do
   args <- getArgs
-  contents <- readFile (head args)
-  let values = reform $ parse contents
-  print (sum $ sortedDiffs values)
-  print (sum $ similarity (countOccurances $ fst values) $ snd values)
+  case args of
+    [fileName] -> do
+      contents <- readFile fileName
+      let values = unzip $ parse contents
+      print (uncurry sumSortedDiffs values)
+      print (uncurry similarityScore values)
+    _ -> hPutStrLn stderr "Usage: program <input-file>"
--- README.md
+++ README.md
@@ -20,6 +20,7 @@ runghc [year]/day[day].hs input/[year]/day[day].txt
 
 | Year | Progress |
 | ---- | -------- |
+| 2024 | 1/25     |
 | 2023 | 6/25     |
 | 2022 | 5/25     |
 | 2021 | 1/25     |
